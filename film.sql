ae662313d6dba0233543cfb711945fd2df2c3f4b
nguyenlkh <lkhnguyen3006@gmail.com>
2024-03-02 09:42:16 +0700

    sql

---
+++ film.sql
@@ -0,0 +1,14 @@
+﻿create table film(
+src nvarchar(50),
+ten nvarchar(50),
+theloai nvarchar(50),
+thoigian nvarchar(50),
+khoichieu nvarchar(50),
+tuoi nvarchar(50)
+)
+
+
+insert into film values('images/mai4.jpg','MAI (T18)','TÌNH CẢM','130 PHÚT','10-02-2024','C18')
+insert into film values('images/mai5.jpg','MAI (T18)','TÌNH CẢM','130 PHÚT','10-02-2024','C13')
+insert into film values('images/mai6.jpg','MAI (T18)','TÌNH CẢM','130 PHÚT','10-02-2024','C16')
+insert into film values('images/mai7.jpg','MAI (T18)','TÌNH CẢM','130 PHÚT','10-02-2024','P')
