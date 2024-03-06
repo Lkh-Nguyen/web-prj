c99b02ff041cdd04e5b5f69b578bb610a7a528b4
nguyenlkh <lkhnguyen3006@gmail.com>
2024-03-06 11:26:17 +0700

    hello

--- Test/src/java/Database/DatabaseInfo.java
+++
@@ -1,9 +0,0 @@
-package Database;
-
-
-public interface DatabaseInfo {
-    public static String DRIVERNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static String DBURL = "jdbc:sqlserver://LAPTOP-O852JI22;databaseName=Cinema;encrypt=false;trustServerCertificate=false;loginTimeout=30;characterEncoding=UTF-8;";
-    public static String USERDB="sa";
-    public static String PASSDB="admin";
-}
