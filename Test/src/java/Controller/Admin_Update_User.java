016035e8a5555fc27056e5e45ee2b17945df52d9
hoanghai1910 <hoanghai270623@gmail.com>
2024-03-18 17:27:06 +0700

    update_hoanghai

--- Test/src/java/Controller/Admin_Update_User.java
+++ Test/src/java/Controller/Admin_Update_User.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controller;
 
 import Database.UserDB;
@@ -22,34 +21,37 @@ import java.text.SimpleDateFormat;
  * @author MSI
  */
 public class Admin_Update_User extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Admin_Update_User</title>");  
+            out.println("<title>Servlet Admin_Update_User</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Admin_Update_User at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet Admin_Update_User at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -57,17 +59,18 @@ public class Admin_Update_User extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         int id = Integer.parseInt(request.getParameter("userId"));
         User u = UserDB.getUser(id);
         HttpSession session = request.getSession();
-        request.setAttribute("user", u);
+        request.setAttribute("User", u);
         session.setAttribute("userList", UserDB.getAllUsers());
-       request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
-    } 
+        request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -75,7 +78,7 @@ public class Admin_Update_User extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         String name = request.getParameter("name");
         String gender = request.getParameter("gender");
         String dobStr = request.getParameter("DOB");
@@ -86,8 +89,11 @@ public class Admin_Update_User extends HttpServlet {
         String repassword = request.getParameter("repassword");
         String address = request.getParameter("address");
         String role = request.getParameter("role");
+        PrintWriter out = response.getWriter();
+
         int iRole = role(role);
         int oldId = Integer.parseInt(request.getParameter("userId"));
+
         User oldUser = UserDB.getUser(oldId);
         java.sql.Date dateOfBirth = null;
         try {
@@ -97,39 +103,39 @@ public class Admin_Update_User extends HttpServlet {
             e.printStackTrace(); // Handle parsing exception appropriately
         }
         User newUser = new User(name, gender, dateOfBirth, cmnd, phoneNumber, email, password, address, iRole);
-        if(!email.equals(oldUser.getEmail())&&UserDB.doesUserExist(email)){
-        request.setAttribute("emailError", "Email đã tồn tại!");
-        request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
-        }       
-        else if (!password.equals(repassword)) {
-        request.setAttribute("repasswordError", "Mật khẩu không khớp!");
-        request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
+        if (!email.equals(oldUser.getEmail()) && UserDB.doesUserExist(email)) {
+            request.setAttribute("emailError", "Email đã tồn tại!");
+            request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
+        } else if (!password.equals(repassword)) {
+            request.setAttribute("repasswordError", "Mật khẩu không khớp!");
+            request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
         } // Check password
         else if (UserDB.updateUser(oldUser, newUser)) {
-        HttpSession session = request.getSession();
-        session.setAttribute("userList", UserDB.getAllUsers());
-        request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
-        // Update the session with the new user information
-         } else {
-        HttpSession session = request.getSession();
-        session.setAttribute("userList", UserDB.getAllUsers());
-        request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
-    }
+            HttpSession session = request.getSession();
+            session.setAttribute("userList", UserDB.getAllUsers());
+            request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
+            // Update the session with the new user information
+        } else {
+            HttpSession session = request.getSession();
+            session.setAttribute("userList", UserDB.getAllUsers());
+            request.getRequestDispatcher("full_admin_user.jsp").forward(request, response);
+        }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-     public int role(String role){
-        if(role == "Guest"){
+
+    public int role(String role) {
+        if (role == "Guest") {
             return 1;
-        }
-        else{
+        } else {
             return 2;
         }
     }
